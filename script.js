0f28827a99a3e28c081b44de4ad6e3c275b6d27e
nawab01 <nawabahmad1992@gmail.com>
2024-09-06 12:56:51 -0400

    function examples

--- script.js
+++ script.js
@@ -1,57 +1,21 @@
-const myOldLibrary = []
-const myLibrary = []
 
-let displayedMovies = '';
-let movieTitle;
-let movieDirector;
-let movieYear;
-function Movie(title, director, year) {
-    this.title = title;
-    this.director = director;
-    this.year = year
+// object constructor example
+// function Player(name, marker) {
+//     this.name = name;
+//     this.marker = marker;
+// }
+// const player1 = new Player('john', 'x');
+// console.log(player1.name)
+
+
+//factory functions using arrow func example
+// const Player = (name, marker) => ({ name, marker })
+// const player1 = Player('john', 'x');
+// console.log(player1.name)
+
+//regular factory function
+function Player(name, marker) {
+    return{ name, marker }
 }
-let movie1 = new Movie('Fight Club', 'David Fincher', '1999');
-
-let movie2 = new Movie('Memento', 'Christopher Nolan', '2000');
-
-myOldLibrary.push(movie1);
-myOldLibrary.push(movie2);
-
-for (let i = 0; i < myOldLibrary.length; i++) {
-    let title = myOldLibrary[i].title;
-    let director = myOldLibrary[i].director;
-    let year = myOldLibrary[i].year;
-
-    displayedMovies += title + ', ' + director + ', ' + year + '\n'
-}
-oldLibrary.innerText = displayedMovies;
-
-function addBookToLibrary() {
-    let newMovie = new Movie(movieTitle, movieDirector, movieYear);
-    myLibrary.push(newMovie);
-    updateLibrary()
-}
-
-function updateLibrary() {
-    let allMovies = '';
-    for (let i = 0; i < myLibrary.length; i++) {
-        movieTitle = myLibrary[i].title;
-        movieDirector = myLibrary[i].director;
-        movieYear = myLibrary[i].year;
-    
-        allMovies = movieTitle + ', ' + movieDirector + ', ' + movieYear + '\n';
-    }
-    libraryContainer.innerText += allMovies;
-}
-
-function submitForm(event) {
-    event.preventDefault();
-    movieTitle = document.getElementById('title').value;
-    movieDirector = document.getElementById('director').value;
-    movieYear = document.getElementById('year').value;
-    addBookToLibrary(movieTitle, movieDirector, movieYear);
-    document.getElementById('movieForm').reset();
-}
-
-document.getElementById('movieForm').addEventListener('submit', submitForm);
-
+const player1 = Player('john', 'x');
+console.log(player1.name)
