af6355b09f4e0bd0c866c8f27c0b806264b60c15
nawab01 <nawabahmad1992@gmail.com>
2024-09-08 15:39:27 -0400

    wrapped everything inside iife's

--- script.js
+++ script.js
@@ -1,33 +1,33 @@
 const playGame = (() => {
-let count = 0;
-const boxes = Array.from(document.getElementsByClassName('box'));
-console.log(boxes[0].innerText);
-const changeColor = (e) => {
-    let targetIndex = e.target.id
-    console.log(targetIndex);
-    if (boxes[targetIndex].innerText === '') {
-        if (count % 2 == 0) {
-        boxes[targetIndex].innerText = 'X'
-    }
-    if (count % 2 != 0) {
-        boxes[targetIndex].innerText = 'O'
-    }
-   }
+    let count = 0;
+    const boxes = Array.from(document.getElementsByClassName('box'));
 
-   console.log(boxes[0].innerText);
+    const changeColor = (e) => {
+        const targetIndex = e.target.id; 
+        console.log(targetIndex);
+            if (boxes[targetIndex].innerText === '') {
+                if (count % 2 === 0) {
+                    boxes[targetIndex].innerText = 'X';
+                } else {
+                    boxes[targetIndex].innerText = 'O';
+                }
+                count++;
+            } else {
+                console.log('Box is already marked.');
+            }
 
-       count++
-}
-return {changeColor, boxes};
+
+        console.log(boxes[0].innerText);
+    };
+
+    return { changeColor, boxes };
 
 })();
 
 const render = (() => {
-    const {changeColor} = playGame;
-    const {boxes} = playGame;
+    const { changeColor, boxes } = playGame; 
 
-boxes.forEach(box => {
-    box.addEventListener('click', changeColor);
-});
+    boxes.forEach(box => {
+        box.addEventListener('click', changeColor);
+    });
 })();
-
